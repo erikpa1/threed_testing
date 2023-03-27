95e9cce4c21c26cbd245dd87c982d6aad768e82e
erik.palencik <erik.palencik@exe.sk>
2023-03-27 20:34:32 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ pub async fn run() {
         1000.0,
     );
     let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
+
     let mut gui = three_d::GUI::new(&context);
 
     let mut loaded = if let Ok(loaded) = three_d_asset::io::load_async(&[
